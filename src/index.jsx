8e03dc245367ea0880eb41a4ba357092d85b01eb
Valentina Mackovic <valentina.mackovic@pbssolutions.net>
2019-12-16 16:54:46 +0100

    Create Windows installer

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
-import './appEntry.scss';
-import App from './App.jsx';
-import 'bootstrap';
-
-const app = document.getElementById('app');
-ReactDOM.render(<HashRouter><App/></HashRouter>, app);

6fba12eefbb313706c970f62cde9dd3347624da0
Valentina Mackovic <valentina.mackovic@pbssolutions.net>
2019-12-16 16:54:46 +0100

    Electron-packager creating blank page

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const dbHelper = require('./src/database/dbHelper');
+require('electron-debug')();
+
+//windows installer 
+if (handleSquirrelEvent(app)) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
 
 let window;
 
@@ -29,7 +36,18 @@ function createWindow() {
         width: 'max',
         height: 'max'
     });
-    window.loadURL('http://localhost:3000');
+    //window.loadURL('http://localhost:3000');
+
+    //building url
+    const url = require('url')
+    const path = require('path');
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, 'public/index.html'),
+        protocol: 'file:',
+        slashes: true,
+        files: ["src/*", "./*"]
+      }));
+
     window.on('closed', () => {
         window = null;
     });
@@ -62,3 +80,70 @@ function databaseOperations() {
 
 app.on('ready', databaseOperations);
 app.on('ready', createWindow);
+
+
+
+//windows isntaller
+function handleSquirrelEvent(application) {
+    if (process.argv.length === 1) {
+        return false;
+    }
+
+    const ChildProcess = require('child_process');
+    const path = require('path');
+
+    const appFolder = path.resolve(process.execPath, '..');
+    const rootAtomFolder = path.resolve(appFolder, '..');
+    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+    const exeName = path.basename(process.execPath);
+
+    const spawn = function(command, args) {
+        let spawnedProcess, error;
+
+        try {
+            spawnedProcess = ChildProcess.spawn(command, args, {
+                detached: true
+            });
+        } catch (error) {}
+
+        return spawnedProcess;
+    };
+
+    const spawnUpdate = function(args) {
+        return spawn(updateDotExe, args);
+    };
+
+    const squirrelEvent = process.argv[1];
+    switch (squirrelEvent) {
+        case '--squirrel-install':
+        case '--squirrel-updated':
+            // Optionally do things such as:
+            // - Add your .exe to the PATH
+            // - Write to the registry for things like file associations and
+            //   explorer context menus
+
+            // Install desktop and start menu shortcuts
+            spawnUpdate(['--createShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-uninstall':
+            // Undo anything you did in the --squirrel-install and
+            // --squirrel-updated handlers
+
+            // Remove desktop and start menu shortcuts
+            spawnUpdate(['--removeShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-obsolete':
+            // This is called on the outgoing version of your app before
+            // we update to the new version - it's the opposite of
+            // --squirrel-updated
+
+            application.quit();
+            return true;
+    }
+};
